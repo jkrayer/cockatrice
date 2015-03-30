249eca1705870932b3a0391a3790488db5779608
jkrayer <jameskrayer@yahoo.com>
2015-03-30 12:32:36 -0400

    Add NoteAddCtrl

---
+++ js/app/ctrl/notes-add.js
@@ -0,0 +1,10 @@
+//
+var app = app || angular.module('CharacterApp', ['ngRoute']);
+
+
+//controllers
+app.controller('NotesAddCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+
+
+
+}]);
